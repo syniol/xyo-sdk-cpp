311e9874e52c5b6662779b8bddfa147a21843f32
Hadi T <hadi@syniol.com>
2026-07-10 22:57:33 +0100

    fix: pipeline errors final iteration

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,8 +9,8 @@ class TestPackageConan(ConanFile):
 
     def requirements(self):
         self.requires(self.tested_reference_str)
-
-
+    def layout(self):
+        cmake_layout(self)
     def build(self):
         cmake = CMake(self)
         cmake.configure()
